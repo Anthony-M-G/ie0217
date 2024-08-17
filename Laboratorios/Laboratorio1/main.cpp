2d60810409ad7feb11126fa16555b59d6066156a
Antony <antony.medinagarcia24@gmail.com>
2024-08-16 18:41:56 -0600

    laboratorio 1
    Signed-off-by: Antony <antony.medinagarcia24@gmail.com>

---
+++ Laboratorios/Laboratorio1/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+    int number;
+    cout << "Enter a integer: "<< endln;
+    cin >> number;
+
+    cout << "Your number: "<< number << endln;
+    return 0;
+}
