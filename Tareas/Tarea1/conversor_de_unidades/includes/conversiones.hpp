6aeda7e2af38d27481de6721ebf3c222511042b4
Antony <antony.medinagarcia24@gmail.com>
2024-08-23 15:18:35 -0600

    add: declaración y definición de funciones de conversión

---
+++ Tareas/Tarea1/conversor_de_unidades/includes/conversiones.hpp
@@ -0,0 +1,42 @@
+#ifndef CONVERSIONES_METROS_HPP
+#define CONVERSIONESMETROS_HPP
+// Conversiones de unidades de longitud
+double metros_a_kilometros(double valor);
+
+double metros_a_centimetros(double valor);
+
+double metros_a_milimetros(double valor);
+
+double kilometros_a_metros(double valor);
+
+double centimetros_a_metros(double valor);
+
+double milimetros_a_metros(double valor);
+
+// Conversiones de unidades de temperatura
+double celsius_a_fahrenheit(double valor);
+
+double celsius_a_kelvin(double valor);
+
+double fahrenheit_a_celsius(double valor);
+
+double fahrenheit_a_kelvin(double valor);
+
+double kelvin_a_celsius(double valor);
+
+double kelvin_a_fahrenheit(double valor);
+
+// Conversiones de unidades de masa
+double kilogramos_a_gramos(double valor);
+
+double kilogramos_a_miligramos(double valor);
+
+double gramos_a_kilogramos(double valor);
+
+double gramos_a_miligramos(double valor);
+
+double miligramos_a_kilogramos(double valor);
+
+double miligramos_a_gramos(double valor);
+
+#endif
