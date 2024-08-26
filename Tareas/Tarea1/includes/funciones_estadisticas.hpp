c887113fd13e48114bb74fccc2a30ffa3469ec8e
Antony <antony.medinagarcia24@gmail.com>
2024-08-26 08:02:22 -0600

    add: declaración de funciones estadísticas

---
+++ Tareas/Tarea1/includes/funciones_estadisticas.hpp
@@ -0,0 +1,9 @@
+#ifndef FUNC_STATS_HPP
+#define FUNC_STATS_HPP
+// Declaración de funciones estadisticas
+void suma(int *, int cantidad_numeros);
+void promedio(int *, int cantidad_numeros);
+void maximo(int *, int cantidad_numeros);
+void minimo(int *, int cantidad_numeros);
+
+#endif // FUNC_STATS_HPP
