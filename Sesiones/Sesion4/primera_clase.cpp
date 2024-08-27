5d67b31c7555f2f890d34a9620511ce5f2d67a5f
Antony <antony.medinagarcia24@gmail.com>
2024-08-27 16:44:04 -0600

    add: añadiendo destructor

--- Sesiones/Sesion4/primera_clase.cpp
+++ Sesiones/Sesion4/primera_clase.cpp
@@ -8,9 +8,16 @@ public:
   double ancho;
   double alto;
 
-  Molde() // Constructor de la clase
+  Molde(double t_largo, double t_ancho, double t_alto) // Constructor de la clase
   {
+    largo = t_largo;
+    ancho = t_ancho;
+    alto = t_alto;
     cout << "Se ha creado un objeto de la clase Molde" << endl;
+  }
+  ~Molde()
+  {
+    cout << "Se ha destruido un objeto de la clase Molde" << endl;
   }
 
   double calcularArea()
@@ -26,10 +33,7 @@ public:
 
 int main()
 {
-  Molde pared; // Instancia de la clase Molde llamada pared
-  pared.largo = 10;
-  pared.ancho = 5;
-  pared.alto = 3;
+  Molde pared(10.0, 20.0, 35.0); // Instancia de la clase Molde llamada pared
 
   cout << "El area de la pared es: " << pared.calcularArea() << endl;
   cout << "El volumen de la pared es: " << pared.calcularVolumen() << endl;
