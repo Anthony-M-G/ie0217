a7a8f1e4941e57faa810cd93cc3ff4c4b5616a73
Antony <antony.medinagarcia24@gmail.com>
2024-09-03 14:54:34 -0600

    explicación de la palabra reservada new

--- Sesiones/Sesion5/heap_stack_variables.cpp
+++ Sesiones/Sesion5/heap_stack_variables.cpp
@@ -9,6 +9,7 @@ int main()
   int stackVariable = 10;
 
   /* Se almacena en el heap. */
+  // New me devuelve un puntero al espacio de memoria asignado en el heap
   int *heapVariable = new int(20);
 
   std::cout << "Valor de globalVariable: " << globalVariable << std::endl;
@@ -16,6 +17,7 @@ int main()
   std::cout << "Valor de heapVariable: " << *heapVariable << std::endl;
 
   // Liberar la memoria asignada en el heap, necesario para evitar fugas de memoria.
+  // Cuando usamos new, debemos liberar la memoria con delete
   delete heapVariable;
 
   return 0;
