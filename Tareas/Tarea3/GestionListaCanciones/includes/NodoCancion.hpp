428443188df828499a9fd4b937b9c57172778e8a
Antony <antony.medinagarcia24@gmail.com>
2024-09-09 17:35:37 -0600

    update: comentarios sobre el código

--- Tareas/Tarea3/GestionListaCanciones/includes/NodoCancion.hpp
+++ Tareas/Tarea3/GestionListaCanciones/includes/NodoCancion.hpp
@@ -1,7 +1,7 @@
 #ifndef NODOCANCION_HPP
 #define NODOCANCION_HPP
 #include "Cancion.hpp"
-
+// Nodo de la lista de canciones que contiene un puntero a la siguiente canción y la canción que aloja
 typedef struct NodoCancion
 {
   Cancion cancion;
--- Tareas/Tarea3/GestionListaCanciones/src/ListaCanciones.cpp
+++ Tareas/Tarea3/GestionListaCanciones/src/ListaCanciones.cpp
@@ -4,9 +4,9 @@ ListaCanciones::ListaCanciones() : nodoCabeza(nullptr) {}
 
 void ListaCanciones::insertarAlInicio(Cancion *&cancion)
 {
-  NodoCancion *nuevoNodo = new NodoCancion(*cancion);
-  nuevoNodo->siguiente = nodoCabeza;
-  nodoCabeza = nuevoNodo;
+  NodoCancion *nuevoNodo = new NodoCancion(*cancion); // Creamos un nuevo nodo con la canción
+  nuevoNodo->siguiente = nodoCabeza;                  // El siguiente del nuevo nodo es la cabeza de la lista
+  nodoCabeza = nuevoNodo;                             // La cabeza de la lista es el nuevo nodo
   std::cout << "Nueva canción agregada al inicio correctamente" << std::endl;
 };
 
@@ -14,46 +14,77 @@ void ListaCanciones::insertarAlFinal(Cancion *&cancion)
 {
   NodoCancion *nuevoNodo = new NodoCancion(*cancion);
   NodoCancion *tmp = nodoCabeza;
-  if (nodoCabeza == nullptr)
+  if (nodoCabeza == nullptr) // Si la lista está vacía se agrega como la primera
   {
     nodoCabeza = nuevoNodo;
     nodoCabeza->siguiente = nullptr;
     std::cout << "La lista estaba vacía por lo cual se agregó como la primera en la lista" << std::endl;
     return;
   }
-  while (tmp->siguiente != nullptr)
+  while (tmp->siguiente != nullptr) // Se recorre la lista hasta llegar al último nodo
+  {
+    tmp = tmp->siguiente; // Se avanza al siguiente nodo
+  }
+  tmp->siguiente = nuevoNodo;     // Se agrega el nuevo nodo al final
+  nuevoNodo->siguiente = nullptr; // El siguiente del nuevo nodo es nulo
+};
+
+void ListaCanciones::insertarEn(Cancion *&cancion, int pos)
+{
+  NodoCancion *nuevoNodo = new NodoCancion(*cancion);
+  NodoCancion *tmp = nodoCabeza;
+  int i = 0;
+  if (nodoCabeza == nullptr) // Si la lista está vacía se agrega como la primera
+  {
+    nodoCabeza = nuevoNodo;
+    nodoCabeza->siguiente = nullptr;
+    std::cout << "La lista estaba vacía por lo cual se agregó como la primera en la lista" << std::endl;
+    return;
+  }
+  if (pos == 0) // Si la posición es 0 se agrega como la primera
+  {
+    nuevoNodo->siguiente = nodoCabeza;
+    nodoCabeza = nuevoNodo;
+    return;
+  }
+  while (tmp != nullptr && i < pos - 1) // Se recorre la lista hasta llegar a la posición n-1
   {
     tmp = tmp->siguiente;
   }
-  tmp->siguiente = nuevoNodo;
-  nuevoNodo->siguiente = nullptr;
-};
-
-void ListaCanciones::insertarEn(Cancion *&cancion, int pos) {
-
+  if (tmp == nullptr) // Si no encontramos el nodo en la posición n-1
+  {
+    // Si no encontramos el nodo en la posición n-1
+    std::cout << "La posición es mayor que el tamaño de la lista" << std::endl;
+    delete nuevoNodo; // Liberar la memoria del nodo si no se va a usar
+    return;
+  }
+  nuevoNodo->siguiente = tmp->siguiente; // El siguiente del nuevo nodo es el siguiente del nodo en la posición n-1
+  tmp->siguiente = nuevoNodo;            // El siguiente del nodo en la posición n-1 es el nuevo nodo
 };
 
 void ListaCanciones::listarCanciones() const
 {
-  if (nodoCabeza == nullptr)
+  int numeroCancion = 1;
+  if (nodoCabeza == nullptr) // Si la lista está vacía se imprime un mensaje
   {
     std::cout << "La lista esta vacía" << std::endl;
   }
   NodoCancion *tmp = nodoCabeza;
-  while (tmp != nullptr)
+  while (tmp != nullptr) // Se recorre la lista imprimiendo las canciones
   {
-    std::cout << "Canción: " << tmp->cancion.getNombre() << " Artista: " << tmp->cancion.getArtista() << " Duración: " << tmp->cancion.getDuracion() << std::endl;
+    std::cout << numeroCancion << "." << " Canción: " << tmp->cancion.getNombre() << " Artista: " << tmp->cancion.getArtista() << " Duración: " << tmp->cancion.getDuracion() << std::endl;
     tmp = tmp->siguiente;
+    numeroCancion++;
   }
 };
 
 void ListaCanciones::eliminarCancion(const std::string &nombre)
 {
-  if (nodoCabeza == nullptr)
+  if (nodoCabeza == nullptr) // Si la lista está vacía se imprime un mensaje
   {
     std::cout << "La lista esta vacía" << std::endl;
   }
-  if (nodoCabeza->cancion.getNombre() == nombre)
+  if (nodoCabeza->cancion.getNombre() == nombre) // Si la canción a eliminar es la primera
   {
     NodoCancion *tmp = nodoCabeza;
     nodoCabeza = nodoCabeza->siguiente;
@@ -78,27 +109,27 @@ void ListaCanciones::eliminarCancion(const std::string &nombre)
 void ListaCanciones::modificar(const std::string &nombreBusqueda,
                                const std::string &nuevo_nombre,
                                const std::string &artista,
-                               double nuevaDuracion)
+                               double nuevaDuracion) // Modifica una canción
 {
   NodoCancion *tmp = nodoCabeza;
-  if (nodoCabeza == nullptr)
+  if (nodoCabeza == nullptr) // Si la lista está vacía se imprime un mensaje
   {
     std::cout << "La lista esta vacía" << std::endl;
   }
-  while (tmp != nullptr && tmp->cancion.getNombre() != nombreBusqueda)
+  while (tmp != nullptr && tmp->cancion.getNombre() != nombreBusqueda) // Se recorre la lista hasta encontrar la canción
   {
     tmp = tmp->siguiente;
   }
-  if (tmp == nullptr)
+  if (tmp == nullptr) // Si no se encontró la canción se imprime un mensaje
   {
     std::cout << "No se encontró la canción" << std::endl;
     return;
   }
-  if (nuevo_nombre != "")
+  if (nuevo_nombre != "") // Se modifica el nombre si es diferente de vacío
     tmp->cancion.setNombre(nuevo_nombre);
-  if (artista != "")
+  if (artista != "") // Se modifica el artista si es diferente de vacío
     tmp->cancion.setArtista(artista);
-  if (nuevaDuracion != 0)
+  if (nuevaDuracion != 0) // Se modifica la duración si es diferente de 0
     tmp->cancion.setDuracion(nuevaDuracion);
 };
 
