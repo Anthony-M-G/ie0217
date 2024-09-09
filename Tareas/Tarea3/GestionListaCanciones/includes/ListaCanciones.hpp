06cdf892c57b4a375618d51ba13967e4bd8ad228
Antony <antony.medinagarcia24@gmail.com>
2024-09-09 17:35:15 -0600

    update: comentarios sobre el código

--- Tareas/Tarea3/GestionListaCanciones/includes/ListaCanciones.hpp
+++ Tareas/Tarea3/GestionListaCanciones/includes/ListaCanciones.hpp
@@ -11,11 +11,17 @@ public:
   ListaCanciones(/* args */);
   ~ListaCanciones();
 
+  // Inserta Nodo al inicio de la lista
   void insertarAlInicio(Cancion *&cancion);
+  // Inserta Nodo al final de la lista
   void insertarAlFinal(Cancion *&cancion);
+  // Inserta Nodo en una posición específica
   void insertarEn(Cancion *&cancion, int pos);
+  // Lista las canciones
   void listarCanciones() const;
+  // Elimina una canción
   void eliminarCancion(const std::string &nombre);
+  // Modifica una canción
   void modificar(const std::string &nombreBusqueda, const std::string &nuevo_nombre, const std::string &artista, double nuevaDuración);
 };
 
