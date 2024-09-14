c5923b645511aef3b6106b9c4336b6436ba34423
Antony <antony.medinagarcia24@gmail.com>
2024-09-13 19:03:55 -0600

    add: declaración de funciones de ordenamiento

---
+++ Laboratorios/Laboratorio5/includes/SortAlgorithms.hpp
@@ -0,0 +1,16 @@
+#ifndef SORT_ALGORITHMS_HPP
+#define SORT_ALGORITHMS_HPP
+
+#include <vector>
+using namespace std;
+
+// Declaración de Bubble Sort
+void bubble_sort(vector<int> &arr);
+
+// Declaración de Selection Sort
+void selection_sort(vector<int> &arr);
+
+// Declaración de Insertion Sort
+void insertion_sort(vector<int> &arr);
+
+#endif // SORT_ALGORITHMS_HPP
