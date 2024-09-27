03dda76264a75ed776b565f950108941e17dca5e
Antony <antony.medinagarcia24@gmail.com>
2024-09-27 13:06:44 -0600

    add: comentarios

--- Tareas/Tarea4/GestorDeProyectos/src/GestorDeProyectos.cpp
+++ Tareas/Tarea4/GestorDeProyectos/src/GestorDeProyectos.cpp
@@ -9,19 +9,25 @@ GestorDeProyectos::~GestorDeProyectos() {}
 
 void GestorDeProyectos::agregarProyecto(Proyecto &proyecto) {
   try {
-    listaDeProyectos[proyecto.getNombre()] = proyecto;
+    listaDeProyectos[proyecto.getNombre()] =
+        proyecto; // Se agrega el proyecto al map de proyectos
     std::cout << "Proyecto agregado con éxito" << std::endl;
-  } catch (const std::invalid_argument &e) {
+  } catch (const std::invalid_argument
+               &e) { // Se captura la excepción de std::invalid_argument
     std::cerr << e.what() << '\n';
   }
 }
 
 void GestorDeProyectos::eliminarTareaDeProyecto(
     const std::string &nombreProyecto, const std::string &nombreTarea) {
-  auto proyecto = listaDeProyectos.find(nombreProyecto);
+  auto proyecto =
+      listaDeProyectos.find(nombreProyecto); // Se busca el proyecto en el map
 
-  if (proyecto != listaDeProyectos.end()) {
-    proyecto->second.eliminarTarea(nombreTarea);
+  if (proyecto !=
+      listaDeProyectos
+          .end()) { // Si el proyecto existe en el map se elimina la tarea
+    proyecto->second.eliminarTarea(
+        nombreTarea); // Se llama al método eliminarTarea de la clase Proyecto
     std::cout << "Tarea eliminada del proyecto: "
               << proyecto->second.getNombre() << std::endl;
     return;
@@ -29,6 +35,7 @@ void GestorDeProyectos::eliminarTareaDeProyecto(
   throw NotFoundException("Proyecto no encontrado");
 }
 
+// Se agrega la tarea al proyecto
 void GestorDeProyectos::agregarTareaAProyecto(const std::string &nombre,
                                               BaseTarea *&tarea) {
   auto proyecto = listaDeProyectos.find(nombre);
@@ -42,6 +49,7 @@ void GestorDeProyectos::agregarTareaAProyecto(const std::string &nombre,
   throw NotFoundException("Proyecto no encontrado");
 }
 
+// Se elimina el proyecto del map
 void GestorDeProyectos::eliminarProyecto(const std::string &nombre) {
   auto proyecto = listaDeProyectos.find(nombre);
 
@@ -53,6 +61,7 @@ void GestorDeProyectos::eliminarProyecto(const std::string &nombre) {
   throw NotFoundException("Proyecto no encontrado");
 }
 
+// Se llama al método mostrarResumen de la clase Proyecto
 void GestorDeProyectos::mostrarResumenTareas(const std::string &nombre) const {
   auto proyecto = listaDeProyectos.find(nombre);
 
@@ -63,13 +72,15 @@ void GestorDeProyectos::mostrarResumenTareas(const std::string &nombre) const {
   throw NotFoundException("Proyecto no encontrado");
 }
 
+// Se calcula el costo total de las tareas del proyecto
 void GestorDeProyectos::calcularCostoTotal(const std::string &nombre) const {
   auto proyecto = listaDeProyectos.find(nombre);
 
   if (proyecto != listaDeProyectos.end()) {
-    double costo_total = 0;
+    double costo_total = 0; // Se inicializa el costo total en 0
     for (auto &tarea : proyecto->second.getListaDeTareas()) {
-      costo_total += tarea->getCosto();
+      costo_total +=
+          tarea->getCosto(); // Se suma el costo de cada tarea al costo total
     }
     std::cout << "Proyecto: " << proyecto->second.getNombre()
               << " Costo total: " << costo_total << std::endl;
@@ -78,6 +89,7 @@ void GestorDeProyectos::calcularCostoTotal(const std::string &nombre) const {
   throw NotFoundException("Proyecto no encontrado");
 }
 
+// Se calcula el tiempo total estimado de las tareas del proyecto
 void GestorDeProyectos::calcularTiempoTotal(const std::string &nombre) const {
   auto proyecto = listaDeProyectos.find(nombre);
 
@@ -93,6 +105,7 @@ void GestorDeProyectos::calcularTiempoTotal(const std::string &nombre) const {
   throw NotFoundException("Proyecto no encontrado");
 }
 
+// Se llama al método ordenarTareasPorCosto de la clase Proyecto
 void GestorDeProyectos::ordenarTareasDeProyectoPorCosto(
     const std::string &nombre) const {
   auto proyecto = listaDeProyectos.find(nombre);
@@ -104,6 +117,7 @@ void GestorDeProyectos::ordenarTareasDeProyectoPorCosto(
   throw NotFoundException("Proyecto no encontrado");
 }
 
+// Se llama al método ordenarTareasPorPrioridad de la clase Proyecto
 void GestorDeProyectos::ordenarTareasDeProyectoPorPrioridad(
     const std::string &nombre) const {
   auto proyecto = listaDeProyectos.find(nombre);
@@ -115,6 +129,7 @@ void GestorDeProyectos::ordenarTareasDeProyectoPorPrioridad(
   throw NotFoundException("Proyecto no encontrado");
 }
 
-std::map<std::string, Proyecto> GestorDeProyectos::getListaDeProyectos() const {
+std::map<std::string, Proyecto> GestorDeProyectos::getListaDeProyectos()
+    const { // Se retorna el map de proyectos
   return this->listaDeProyectos;
 }
